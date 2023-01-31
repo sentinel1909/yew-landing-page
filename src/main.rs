48df3176fdbbd35f5127a2049607c9adabcd4383
Jeff Mitchell <sentinel1909@protonmail.com>
2023-01-30 20:58:48 -0800

    Complete basic landing page, update deployment

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,18 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <>
-            <h1><strong>{ "The biggest startup event of the year" }</strong></h1>
-            <button>{ "Find out more" }</button>
-        </>
+        <div class={classes!("container", "d-flex", "align-items-center", "justify-content-center", "h-100")}>
+            <div class={classes!("row")}>
+                <header class={classes!("text-center", "col-12")}>
+                    <h1 class={classes!("text-uppercase")}><strong>{ "The biggest startup event of the year" }</strong></h1>
+                </header>
+                <div class={classes!("buffer", "col-12")}></div>
+                    <section class={classes!("text-center", "col-12")}>
+                        <hr />
+                        <button class={classes!("btn", "btn-primary", "btn-x1")}>{ "Find out more" }</button>
+                    </section>
+            </div>
+        </div>
     }
 }
 
